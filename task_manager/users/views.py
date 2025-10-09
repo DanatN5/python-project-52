4cb3dc629314079445485f003cbf754c678cb09b
Danat Nurmambetov <danat-1998@mail.ru>
2025-10-09 10:14:08 +0300

    Add users view

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from task_manager.users.models import User
 
 # Create your views here.
-def index(request):
-    return HttpResponse('users')
+class UsersView(ListView):
+    model = User
+    context_object_name = 'users'
