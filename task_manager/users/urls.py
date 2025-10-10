543bbc5cd128b5d9f29d96c55108f66ffa5678c4
Danat Nurmambetov <danat-1998@mail.ru>
2025-10-10 10:15:59 +0300

    Add CRUD operations

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,4 +4,7 @@ from task_manager.users import views
 
 urlpatterns = [
     path("", views.UsersView.as_view(), name='user_list'),
+    path("create/", views.SignUpUser.as_view(), name='sign_up_user'),
+    path("<int:pk>/update/", views.UpdateUser.as_view(), name='update_user'),
+    path("<int:pk>/delete/", views.DeleteUser.as_view(), name='delete_user'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from task_manager.users.models import User
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 class UsersView(ListView):
     model = User
     context_object_name = 'users'
+
+class SignUpUser(CreateView):
+    model = User
+    fields = ['username', 'password', 'first_name', 'last_name']
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('user_list')
+
+class UpdateUser(UpdateView):
+    model = User
+    fields = ['__all__']
+    template_name = 'users/update.html'
+    context_object_name = 'user'
+    success_url = reverse_lazy('user_list')
+
+class DeleteUser(DeleteView):
+    model = User
+    template_name = 'users/delete.html'
+    context_object_name = 'user'
+    success_url = reverse_lazy('user_list')
